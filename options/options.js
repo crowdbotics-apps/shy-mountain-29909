0e52e6e2f5430cc6570d82f766522957789b2ab9
Crowdbotics <team@crowdbotics.com>
2021-08-18 14:15:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shy_mountain_29909Identifier",
+  url: "https://shy_mountain_29909Identifier.botics.co",
+  api: "https://shy_mountain_29909Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
